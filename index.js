1c984e717a35a2a0d259af2ba0786303bba22832
Oscar Merlos <oscar.merlos.arias@gmail.com>
2017-10-16 17:56:02 -0700

    English trainned data added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cv = require('opencv4nodejs');
 const SWT = require('./src/StrokeWidthTransform');
+const Tes = require('tesseract.js')
 
 const loadByteImage = name => {
 	let image;
@@ -14,6 +15,7 @@ const loadByteImage = name => {
 
 const mainTextDetection = args => {
 	let byteQueryImage = loadByteImage(args[0]);
+	let initTime = (new Date()).getTime();
 	
 	if (byteQueryImage.empty) {
 		console.error("Error while loading image");
@@ -23,6 +25,22 @@ const mainTextDetection = args => {
 	let output = SWT.textDetection(byteQueryImage, true);
 	if (output){
 		cv.imwrite('./dist/result.jpg', output);
+		let image = require('path').resolve('.', './dist/SWT.png');
+		Tes.recognize(image, {lang: 'eng'})
+			.progress(message => {
+				//if (message.status == "recognizing text")
+				//console.log(parseInt(message.progress*100)+"%")
+			})
+			.catch(err => console.error(err))
+			.then(result => {
+				//console.log("FINISHED!")
+				//console.log(result.text)
+			})
+			.finally(resultOrError => {
+				console.log(resultOrError.text);
+				console.log("Finished in: " + (((new Date()).getTime() - initTime) / 1000) + " seconds\n")
+				process.exit();
+			});
 	}
 };
 
